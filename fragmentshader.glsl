cc3c91a6dfbc17a60f91b5836f58e2b750ef1c64
Terrizen <geurdsts@clarkson.edu>
2014-05-02 07:10:27 -0400

    Create fragmentshader.glsl

---
+++ fragmentshader.glsl
@@ -0,0 +1,13 @@
+float CalcShadowFactor(vec4 LightSpacePos) // calculate the shadow factor of pixels
+{
+    vec3 ProjCoords = LightSpacePos.xyz / LightSpacePos.w;
+    vec2 UVCoords;
+    UVCoords.x = 0.5 * ProjCoords.x + 0.5;
+    UVCoords.y = 0.5 * ProjCoords.y + 0.5;
+    float z = 0.5 * ProjCoords.z + 0.5;
+    float Depth = texture(gShadowMap, UVCoords).x;
+    if (Depth < (z + 0.00001))
+        return 0.5;
+    else
+        return 1.0;
+}
